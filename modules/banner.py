bbf87d3060779ab9939f8230bd0c5e9ea1bd960f
0xCACT2S <81681608+Veto95@users.noreply.github.com>
2025-06-08 04:09:14 -0400

    Update banner.py

--- modules/banner.py
+++ modules/banner.py
@@ -1,4 +1,5 @@
 def banner():
+    print("\033[1;32m")
     print(r"""
 .-.   .-.,---.  .-. .-. .---.   
  \ \ / / | .-'  |  \| |/ .-. )  
@@ -15,3 +16,24 @@ def banner():
  Telegram: HELL SHELL [https://t.me/hacking_hell1]
    GitHub: https://github.com/Veto95/VENO
     """)
+    print("\033[0m")
+
+def get_banner_html():
+    return """
+<pre style="color:#1e90ff;font-weight:bold;font-family:monospace">
+.-.   .-.,---.  .-. .-. .---.   
+ \\ \\ / / | .-'  |  \\| |/ .-. )  
+  \\ V /  | `-.  |   | || | |(_) 
+   ) /   | .-'  | |\\  || | | |  
+  (_)    |  `--.| | |)|\\ `-' /  
+         /( __.'/(  (_) )---'   
+        (__)   (__)    (_)     
+
+             VENO v1.0
+        BUG HUNTING & RECON TOOL
+
+         coder: 0xCACT2S (Veto95)
+ Telegram: HELL SHELL [https://t.me/hacking_hell1]
+   GitHub: https://github.com/Veto95/VENO
+</pre>
+"""
